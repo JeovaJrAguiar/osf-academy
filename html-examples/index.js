97765713800abe3d32b36ac999366bdbc7ade517
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-07-29 14:03:05 -0300

    .

--- html-examples/index.js
+++ html-examples/index.js
@@ -1,5 +1,47 @@
 
+obterPokemons();
 
-fetch("https://")
+let resultado;
 
+async function obterPokemons() {
+    const listaPokemons = document.getElementById('lista-pokemons');
 
+    const loading = document.createElement('li');
+    loading.innerText = 'Carregando ...';
+    loading.id = 'loading';
+    listaPokemons.appendChild(loading);
+
+    resultado = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const { count, results } = await resultado.json();
+
+    loading.remove();
+
+    document.getElementById("count").innerText = `Count: ${count}`;
+    results.forEach(({ name }) => {
+        const item = document.createElement("li");
+        item.innerText = name;
+        listaPokemons.appendChild(item);
+    });
+}
+
+async function obterProxPokemons(){
+    const listaPokemons = document.getElementById('lista-pokemons');
+
+    const loading = document.createElement('li');
+    
+    const resultado = await fetch(resultado.next);
+    const { count, results } = await resultado.json();
+
+    loading.remove();
+
+    document.getElementById("count").innerText = `Count: ${count}`;
+    results.forEach(({ name }) => {
+        const item = document.createElement("li");
+        item.innerText = name;
+        listaPokemons.appendChild(item);
+    });
+}
+
+async function obterAntPokemons(){
+    obterPokemons();
+}
