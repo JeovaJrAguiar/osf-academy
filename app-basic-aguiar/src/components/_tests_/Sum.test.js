d938599efa5a2c04567ab40862320a975745bc01
JeovaJrAguiar <jeova.junior897@gmail.com>
2022-08-05 14:05:49 -0300

    .

---
+++ app-basic-aguiar/src/components/_tests_/Sum.test.js
@@ -0,0 +1,9 @@
+const sum = require('../Sum.js');
+
+test('Quando chamado e passado dois numeros, ele retorna a soma de ambos', () => {
+    const x = Math.random()*10;
+    const y = Math.random()*10;
+
+    expect(sum(x, y)).toBe(x + y);
+}
+);
